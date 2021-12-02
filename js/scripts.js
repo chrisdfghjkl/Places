e374be0a152d75a1f0771b22a2c257041d3ec76f
chrisdfghjkl <cdepastene@gmail.com>
2021-12-01 23:26:02 -0800

    add object function

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Business Logic -----------------
+
+function Places(name, location, date, notes) {
+  this.name = name;
+  this.location = location;
+  this.date = date;
+  this.notes = notes;
+}
