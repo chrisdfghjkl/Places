2b14c658e4ce931808961fa3eee8196cd7f8afb4
chrisdfghjkl <cdepastene@gmail.com>
2021-12-02 23:59:30 -0800

    add ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ function Places(name, location, date, notes) {
 
 // UI Logic ---------------------
 
+$(document).ready(function(){
+  $("form#place-info").submit(function(event) {
+    event.preventDefault();
+    const placeName = $("#name").val();
+    const placeLocation = $("#location").val();
+    const placeDate = $("#when").val();
+    const placeNotes = $("#notes").val();
+
+    let placeLog = new Places(placeName, placeLocation, placeDate, placeNotes);
+    $("#result-log").append("<li style='list-style-type:none;' id='log-title'><h3>" + placeLog.name + "</h3></li><li class='log-item'>" + placeLog.location + "</li><li class='log-item'>" + placeLog.date + "</li><li class='log-item'>" + placeLog.notes + "</li>");
+  });
+});
